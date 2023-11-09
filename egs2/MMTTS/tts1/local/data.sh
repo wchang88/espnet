b33d817078db2b4c81f185d50c1290a1529e6249
Winnie Chang <winniechang88888@gmail.com>
2023-11-09 01:48:22 -0500

    Create data.sh

---
+++ egs2/MMTTS/tts1/local/data.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+log() {
+    local fname=${BASH_SOURCE[1]##*/}
+    echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+SECONDS=0
+
+
+log "Stage 0: Download data and preliminary data prep..."
+
+# download, unzip, data prep, g2p
+# log "\t0.1: Downloading CMU Arctic dataset and prepping"
+# local/download_and_prep_cmu_arctic.sh
+
+# log "\t0.2: Downloading CMU Indic dataset and prepping"
+# local/download_and_prep_cmu_arctic.sh
+
+log "\t0.3: Downloading CSS10 dataset and prepping"
+local/download_and_prep_css10.sh
+
+# log "\t0.4: Downloading CML-TTS dataset and prepping"
+# local/download_and_prep_cmltts.sh
+
+# lof "\t0.5: Downloading Librispeech dataset and prepping"
+# local/download_and_prep_librispeech.sh
